2b7871a3ec6cc6cdeb93654a5d739dcd4202b39d
dhairya <dhairyapatel2702@gmail.com>
2024-03-11 00:02:49 +0530

    implemnted debouncing in search bar
    debouncing is process of reducing number of api calls by using some constraints in this i tried with timeout to reduce it but its not distroying the old timer on creation of new while reconsilliation so i did clear timeout to destroy previous..hence if user types anything before 200ms call will not be made to api

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utils/appSlice';
+import {YOUTUBE_SEARCH_API} from "../utils/constants";
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
+  useEffect(()=>{
+    // getSearchSuggestions();
+    const timer = setTimeout(()=>getSearchSuggestions(),200);
+    return()=>{
+      clearTimeout(timer);
+    }
+  },[searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    try {
+      const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+      const json = await data.json(); // Use await here
+
+      console.log(json[1]);
+    } catch (error) {
+      console.error("Error fetching search suggestions:", error);
+    }
+  };
+
+  
   return (
+    
     <div className='grid grid-flow-col p-2 m-2 shadow-lg'>
 
       <div className='flex colspan-1 items-center'>
@@ -22,10 +45,15 @@ const Head = () => {
         </a>
       </div>
 
+
         <div className='col-span-10'>
-          <input type="text" className='w-1/2 text-center border border-grey-400 p-2 rounded-l-full'/>
+          <input type="text" 
+          value={searchQuery}
+          onChange={(e)=>setSearchQuery(e.target.value)}
+          className='w-1/2 text-center border border-grey-400 p-2 rounded-l-full'/>
           <button className='border border-grey-400 p-2 rounded-r-full bg-gray-100'>Search</button>
         </div>
+
         
         <div className='colspan-1'>
           <img src="https://cdn-icons-png.freepik.com/256/1077/1077114.png" className="h-8" alt ="user-icon"></img>
